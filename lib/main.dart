c439014b2d073293f38fbb14d51ef80d10a83a93
unknown <fredrondina96@gmail.com>
2020-10-25 21:05:50 -0600

    Added rudementary score values to questions

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,11 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -30,26 +33,41 @@ class MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': "What's your favorite colour?",
-      'answers': ['Black', 'Red', 'Purple', 'Yellow']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 20}
+      ]
     },
     {
       'questionText': "What's your favorite animal?",
-      'answers': ['Lizard', 'Dog', 'Cat', 'Bird']
+      'answers': [
+        {'text': 'Cat', 'score': 12},
+        {'text': 'Dog', 'score': 9},
+        {'text': 'Mouse', 'score': 15}
+      ]
     },
     {
       'questionText': "What's your favorite car?",
-      'answers': ['Kia', 'Chevy', 'Mercedes', 'Ford']
+      'answers': [
+        {'text': 'Ford', 'score': 10},
+        {'text': 'Mercedes', 'score': 5},
+        {'text': 'Kia', 'score': 12}
+      ]
     },
   ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('My First App')),
-        body: _questionIndex < _questions.length
-            ? Quiz(answerQuestion: _answerQuestion, questionIndex: _questionIndex, questions: _questions,)
-            : Result()
-      ),
+          appBar: AppBar(title: Text('My First App')),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : Result()),
     );
   }
 }
